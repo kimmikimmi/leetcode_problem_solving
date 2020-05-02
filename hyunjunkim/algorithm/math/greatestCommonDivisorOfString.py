be52a0d7fb6bb8c04bbaec2e4df517acf9dcd1a8
hyunjun kim <bright20136@gmail.com>
2020-05-02 16:05:55 +0900

    Add gcd of string

---
+++ hyunjunkim/algorithm/math/greatestCommonDivisorOfString.py
@@ -0,0 +1,9 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        # print(gcd(1,3))
+        return 1
+
+a = Solution()
+print(a.gcdOfStrings('ABCABC', 'ABC'))
+
+print( 'ABABAB' / 'AB')        
